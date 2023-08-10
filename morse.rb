dc3d0eeed0fc5f8d476ef9254eb9237b91c9b17d
JuanDBta <simplebetsas@gmail.com>
2023-08-10 12:13:59 -0500

    Add method to decode a word

--- morse.rb
+++ morse.rb
@@ -10,3 +10,21 @@ def decode_char(morse_code)
   morse_to_char[morse_code]
 end
 puts decode_char('.-')
+
+=begin
+ Create a method to decode an entire word in Morse code, takes a string parameter, 
+ and return the string representation. 
+ Every character in a word will be separated by a single space (e.g. decode_word("-- -.--") returns "MY").
+=end
+
+def decode_word(str)
+  parts = str.split(" ")
+  word = ""
+  parts.each do |i|
+   letter = decode_char(i)
+   word += letter
+  end
+  puts word
+end
+
+decode_word("-- -.--")
