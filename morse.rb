d9c8c0941068448fcef97188215e04ae4951e7f9
beteland123 <beteland23@gmail.com>
2023-08-10 23:09:25 +0300

    fix linter and update decoder

--- morse.rb
+++ morse.rb
@@ -7,31 +7,24 @@ def decode_char(morse_code)
     '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
     '--..' => 'Z'
   }
-  morse_to_char[morse_code] || ""
+  morse_to_char[morse_code] || ''
 end
 
 def decode_word(str)
-  parts = str.split(" ")
-  word = ""
+  parts = str.split(' ')
+  word = ''
   parts.each do |i|
     letter = decode_char(i)
 
-    if letter
-      word += letter
-    end
+    word += letter if letter
   end
   word
 end
 
-def decode_words(words)
+def decode(words)
   words.split('   ').map { |word| decode_word(word) }.join(' ')
 end
-
-def decode(str)
-  decoded_words = decode_words(str)
-  puts decoded_words
-end
-
-decode("-- -.--   -. .- -- .")
-decode("      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
-
+puts decode_char('.-')
+puts decode_word('-- -.--')
+puts decode('-- -.--   -. .- -- .')
+puts decode('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
